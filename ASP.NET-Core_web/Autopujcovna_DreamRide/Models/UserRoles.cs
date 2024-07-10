497c309633e0e7eeb1532918e58eb1b8ebac4851
Adela_Srbova <a.srbova1@email.cz>
2024-07-10 13:10:20 +0200

    User Roles

---
+++ ASP.NET-Core_web/Autopujcovna_DreamRide/Models/UserRoles.cs
@@ -0,0 +1,8 @@
+﻿namespace Autopujcovna_DreamRide.Models
+{
+    public static class UserRoles
+    {
+        public const string Admin = "admin";
+        public const string RequestManager = "requestManager";
+    }
+}
