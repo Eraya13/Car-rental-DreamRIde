05ee326ea2014bf2d12fe5a275a487e52fde01ab
Adela_Srbova <a.srbova1@email.cz>
2024-07-10 16:56:13 +0200

    Update DbContext
    DbContext ready for scaffolding

--- ASP.NET-Core_web/Autopujcovna_DreamRide/Data/AppDbContext.cs
+++ ASP.NET-Core_web/Autopujcovna_DreamRide/Data/AppDbContext.cs
@@ -7,13 +7,13 @@ namespace Autopujcovna_DreamRide.Data
     public class AppDbContext : IdentityDbContext
     {
         // konstruktor databaze + options - rules nastavení - dodají se v Program.cs
-        public AppDbContext(DbContextOptions<AppDbContext> options)
-            : base(options)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
 
         }
-
-
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Request> Requests { get; set; }
 
     }
 }
