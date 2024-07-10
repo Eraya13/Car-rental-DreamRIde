77451973f8a0e0a008d556c51207a152d2944d9d
Adela_Srbova <a.srbova1@email.cz>
2024-07-10 12:49:10 +0200

    Client Model
    Created model of client for request system

---
+++ ASP.NET-Core_web/Autopujcovna_DreamRide/Models/Client.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Autopujcovna_DreamRide.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Jméno je povinný údaj")]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Příjmení je povinný údaj")]
+        [StringLength(100, ErrorMessage = "Rodné příjmení je dostačující")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Email je povinný údaj")]
+        [EmailAddress(ErrorMessage = "Zadaná adresa není platná")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Je nutné zvolit předvolbu")]
+        public int MobilePreset { get; set; }
+
+        [Required(ErrorMessage = "Je nutné vyplnit telefonní číslo")]
+        [RegularExpression(@"^\d+(\s?\d+)*$", ErrorMessage = "Zadejte platné telefonní číslo.")]
+        [StringLength(20)]      // omezení číslic
+        public string MobileNumber { get; set; }
+    }
+}
