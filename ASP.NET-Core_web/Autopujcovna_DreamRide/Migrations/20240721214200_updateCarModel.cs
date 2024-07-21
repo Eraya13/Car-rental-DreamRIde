8210604c2eb9dc042542a884deb87516c7cfb78c
Adela_Srbova <a.srbova1@email.cz>
2024-07-21 23:56:37 +0200

    Rename of Note in views + Migration of changes Request & Car

---
+++ ASP.NET-Core_web/Autopujcovna_DreamRide/Migrations/20240721214200_updateCarModel.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Autopujcovna_DreamRide.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateCarModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Note",
+                table: "Requests",
+                newName: "AdditionalInfo");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TitleCarImage",
+                table: "Cars",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TitleCarImage",
+                table: "Cars");
+
+            migrationBuilder.RenameColumn(
+                name: "AdditionalInfo",
+                table: "Requests",
+                newName: "Note");
+        }
+    }
+}
